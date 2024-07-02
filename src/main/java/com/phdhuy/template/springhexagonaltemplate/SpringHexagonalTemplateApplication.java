3ca51288df859b3a0bf637bd91a313d6f36f5d93
phduchuy <phanhoangduchuyy@gmail.com>
2024-07-02 12:22:44 +0700

    init codebase

---
+++ src/main/java/com/phdhuy/template/springhexagonaltemplate/SpringHexagonalTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.phdhuy.template.springhexagonaltemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHexagonalTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHexagonalTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phdhuy/template/springhexagonaltemplate/SpringHexagonalTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phdhuy.template.springhexagonaltemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHexagonalTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
